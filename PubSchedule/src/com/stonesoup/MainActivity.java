dae4decaeae36662259bb6d31e5d226f109b378a
GiagkiozisL <giagkiozis.l@hotmail.com>
2013-12-26 15:06:05 +0200

    Imported parse android sdk and internet permissions.

--- PubSchedule/src/com/stonesoup/MainActivity.java
+++ PubSchedule/src/com/stonesoup/MainActivity.java
@@ -1,21 +1,24 @@
 package com.stonesoup;
 
 import android.os.Bundle;
-import android.app.Activity;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+
+import com.parse.ParseAnalytics;
 
 public class MainActivity extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		//track statistics around application opens
+		ParseAnalytics.trackAppOpened(getIntent());
 		setContentView(R.layout.activity_main);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
--- PubSchedule/src/com/stonesoup/StartupActivity.java
+++ PubSchedule/src/com/stonesoup/StartupActivity.java
@@ -3,6 +3,8 @@ package com.stonesoup;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
 
 public class StartupActivity extends Activity{
 
@@ -10,6 +12,7 @@ public class StartupActivity extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 	
+		Parse.initialize(this, "ph2pneKQy22P3wQMnVeufRZfPzf69ZajhyAUH6e1", "5mIvqvTCez5LrynMYzg25A1XXwOXT9YDJUrZ216A");
 		Intent registrationActivity = new Intent(getApplicationContext(),RegistrationActivity.class);
 		startActivity(registrationActivity);
 		finish();
