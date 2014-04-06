0d896bb3ad8c39aa914f30732f144720f7253cd1
GiagkiozisL <giagkiozis.l@hotmail.com>
2014-04-07 00:36:10 +0300

    hockey app sdk

--- PubSchedule/src/com/stonesoup/activities/StartupActivity.java
+++ PubSchedule/src/com/stonesoup/activities/StartupActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.parse.Parse;
+import net.hockeyapp.android.CrashManager;
+import net.hockeyapp.android.UpdateManager;
 
 public class StartupActivity extends Activity{
 
@@ -15,9 +17,23 @@ public class StartupActivity extends Activity{
 		Parse.initialize(this, 
 				"ph2pneKQy22P3wQMnVeufRZfPzf69ZajhyAUH6e1", 
 				"5mIvqvTCez5LrynMYzg25A1XXwOXT9YDJUrZ216A");
-		
+		checkForUpdates();
 		Intent registrationActivity = new Intent(getApplicationContext(),RegistrationActivity.class);
 		startActivity(registrationActivity);
 		finish();
 	}
+	 @Override
+	 protected void onResume() {
+	   super.onResume();
+	   checkForCrashes();
+	 }
+
+	 private void checkForCrashes() {
+	   CrashManager.register(this, "77c16b18981b9e4aeb4534c884d3eea2");
+	 }
+
+	 private void checkForUpdates() {
+	   // Remove this for store builds!
+	   UpdateManager.register(this, "77c16b18981b9e4aeb4534c884d3eea2");
+	 }
 }
